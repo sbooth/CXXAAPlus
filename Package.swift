aed1edc836c72459e74e02fb9bbc5bec8ed3ec59
Stephen F. Booth <me@sbooth.org>
2023-10-18 14:10:05 -0500

    Trailing comma

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
 	name: "CXXAAPlus",
 	platforms: [
 		.macOS(.v10_15),
-		.iOS(.v13)
+		.iOS(.v13),
 	],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to other packages.

f26d2abd0049efadabcdd859b5536875ad5c0176
Stephen F. Booth <me@sbooth.org>
2024-08-31 09:47:33 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,9 @@ let package = Package(
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
 			name: "CXXAAPlus",
-			targets: ["CXXAAPlus"]),
+			targets: [
+				"CXXAAPlus",
+			]),
 	],
 	dependencies: [
 		// Dependencies declare other packages that this package depends on.
@@ -23,11 +25,12 @@ let package = Package(
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
-			name: "CXXAAPlus",
-			dependencies: []),
+			name: "CXXAAPlus"),
 		.testTarget(
 			name: "CXXAAPlusTests",
-			dependencies: ["CXXAAPlus"]),
+			dependencies: [
+				"CXXAAPlus",
+			]),
 	],
 	cxxLanguageStandard: .cxx17
 )
